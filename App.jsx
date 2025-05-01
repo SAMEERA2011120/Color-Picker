2fd4be7d38988c730875aa9df89cde4fb777524d
SAMEERA2011120 <mullasameera66@gmail.com>
2025-05-01 22:02:12 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,9 +1,13 @@
-import ColorPicker from "./ColorPicker.jsx";
+import React from 'react';
+import ColorPicker from './ColorPicker';  // Ensure correct path
+
 function App() {
-  return(
-    <ColorPicker></ColorPicker>
-    
+  return (
+    <div>
+      <ColorPicker />
+    </div>
   );
-  
-  }
-export default App
+}
+
+export default App;
+
